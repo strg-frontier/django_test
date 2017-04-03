66b293723409ea8473fa076dbab6c737d2770c51
strg-frontier <strg.frontier@gmail.com>
2017-04-03 22:56:48 +0900

    add vote and result function

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Poll
+from .models import Choice
 
 # Register your models here.
 
 admin.site.register(Poll)
+admin.site.register(Choice)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils.html import mark_safe
 from .models import Poll
+from .models import Choice
 from django.shortcuts import Http404
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -17,4 +20,24 @@ def detail(request,pk):
     raise Http404
   return render(request, 'polls/detail.html',{
 	'poll' : Poll.objects.get(pk=pk)
+  })
+
+def vote(request, pk):
+  poll = get_object_or_404(Poll,pk=pk)
+  try:
+    selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+  except(KeyError,Choice.DoesNotExist):
+    return render(request, 'polls/detail.html',{
+      'poll':poll,
+      'error_message':"You didn't select a choice",
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return redirect('poll_results',pk)
+
+def results(request,pk):
+  obj = get_object_or_404(Poll,pk=pk)
+  return render(request, 'polls/results.html',{
+    'poll':obj,
   })
