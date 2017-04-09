f17dc91a84c12fdb088383555e34e3ef1c3c169e
strg-frontier <strg.frontier@gmail.com>
2017-04-09 22:00:11 +0900

    enalbe to render with bootstrap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,6 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = {
-#	os.path.join(BASE_DIR, "static"),
-#}
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, "static"),
+)
